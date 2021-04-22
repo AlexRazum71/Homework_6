41b307fd61c0d0aa836c9893af62256f5910f0b3
A. Zhitnikov <alxrzm@yandex.ru>
2021-04-22 11:43:00 +0700

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringSearcher stringSearcher = new();
+
+            List<string> Text = new()
+            {
+                "gdhjhdm",
+                "fhhffjk",
+                "vdvafvf",
+                "nffkdva",
+                "dfv"
+            };
+
+            stringSearcher.Notify += ImFind;
+            stringSearcher.Searcher(Text, "dva");
+
+
+            static void ImFind(string str)
+            {
+                Console.WriteLine($"Строка найдена! Содержимое строки:{str}");
+            }
+            //Console.WriteLine(stringSearcher.Text[3]);
+        }
+    }
+}
---
+++ Task 2/StringSearcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class StringSearcher
+    {
+        public delegate void SearcherHandler(string str);
+        public event SearcherHandler Notify;
+        public void Searcher(List<string> Text, string  str)
+        {
+            if (Text is null)
+            {
+                //throw new ArgumentNullException(nameof(Text));
+                Console.WriteLine("Нет данных для поиска");
+            }
+
+            if (!string.IsNullOrEmpty(str))
+            {
+                for (int i = 0; i < Text.Count; i++)
+                {
+                    if (Text[i].Contains(str))
+                    {
+                        Notify?.Invoke(Text[i]);
+                    } 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Не знаю, что искать!");
+            }
+        }
+    }
+}
